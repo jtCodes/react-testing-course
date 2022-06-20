872a43fd6bede402ca2523fa23bcea6c0ab846d6
jt <jt234wq@gmail.com>
2022-06-20 18:32:43 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import SignUpPage from "./pages/SignUpPage";
-import axios from 'axios';
 
-axios.defaults.proxy.host = "http://localhost:8080"
 function App() {
   return <SignUpPage />;
 }
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -33,22 +33,55 @@ export default function SignUpPage() {
   }
 
   return (
-    <div>
-      <form onChange={onFormChange}>
-        <h1>Sign Up</h1>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="username" value={formData.username}></input>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="email" value={formData.email}></input>
-        <label htmlFor="password">Password</label>
-        <input id="password" type="password" value={formData.password}></input>
-        <label htmlFor="passwordRepeat">Password Repeat</label>
+    <div className="flex justify-center h-screen">
+      <form
+        className="flex flex-col gap-3 p-10 border border-zinc-100 rounded-xl w-[600px] h-max self-center shadow-md"
+        onChange={onFormChange}
+      >
+        <h1 className="text-7xl font-bold text-center text-slate-800 mb-10">
+          Sign Up
+        </h1>
+        <label className="font-semibold" htmlFor="username">
+          Username
+        </label>
         <input
+          className="border rounded-md border-zinc-200 py-2 px-3 text-gray-700 leading-tight"
+          id="username"
+          type="username"
+          value={formData.username}
+        ></input>
+        <label className="font-semibold" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="border rounded-md border-zinc-200 py-2 px-3 text-gray-700 leading-tight"
+          id="email"
+          type="email"
+          value={formData.email}
+        ></input>
+        <label className="font-semibold" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="border rounded-md border-zinc-200 py-2 px-3 text-gray-700 leading-tight"
+          id="password"
+          type="password"
+          value={formData.password}
+        ></input>
+        <label className="font-semibold" htmlFor="passwordRepeat">
+          Password Repeat
+        </label>
+        <input
+          className="border rounded-md border-zinc-200 py-2 px-3 text-gray-700 leading-tight"
           id="passwordRepeat"
           type="password"
           value={formData.passwordRepeat}
         ></input>
-        <button disabled={isSignUpButtonDisabled()} onClick={submit}>
+        <button
+          className="mt-10 bg-sky-500 hover:bg-sky-700 active:bg-sky-900 text-slate-50 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-slate-100 disabled:text-slate-300"
+          disabled={isSignUpButtonDisabled()}
+          onClick={submit}
+        >
           Sign Up
         </button>{" "}
       </form>
